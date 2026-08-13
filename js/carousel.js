6307d10324d83a819266d1b9aece008879601e84
Dust-CRT <saascontapessoal@gmail.com>
2026-08-13 10:31:39 -0300

    Versão final definitiva mesmo o meu deus

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,3 @@
-
-
 //carousel
 
 //Array storage class
@@ -21,13 +19,14 @@ class Carousel {
 
     static Start(arr){
         if(arr){
-
             if(arr.length > 0){
                 carouselArr = arr;
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
                 
                 Carousel.Next(); //start
+                Carousel.RenderDots();
+                Carousel.UpdateDots();
                 Carousel._interval = setInterval(function(){ 
                     Carousel.Next(); 
                 },5000);
@@ -55,6 +54,54 @@ class Carousel {
             carTitleDiv.innerHTML =  `<a href='${item.url}'>${item.title}</a>`
         }
 
+        Carousel.UpdateDots(Carousel._sequence);
         Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
+    }
+
+    static ResetTimer(){
+        clearInterval(Carousel._interval);
+        Carousel._interval = setInterval(function(){
+            Carousel.Next();
+        }, 5000);
+    }
+
+    static Prev(){
+        Carousel._sequence = (Carousel._sequence - 2 + Carousel._size) % Carousel._size;
+        Carousel.Next();
+    }
+
+    static ManualNext(){
+        Carousel.Next();
+        Carousel.ResetTimer();
+    }
+
+    static ManualPrev(){
+        Carousel.Prev();
+        Carousel.ResetTimer();
+    }
+
+    static Goto(index){
+        Carousel._sequence = index;
+        Carousel.Next();
+        Carousel.ResetTimer();
+    }
+
+    static RenderDots(){
+        let dotsHtml = "";
+        for(let i = 0; i < Carousel._size; i++){
+            dotsHtml += "<span class='carousel-dot' onclick='Carousel.Goto(" + i + ")'></span>";
+        }
+        document.getElementById("carousel-dots").innerHTML = dotsHtml;
+    }
+
+    static UpdateDots(currentIndex){
+        let dots = document.querySelectorAll("#carousel-dots .carousel-dot");
+        for(let i = 0; i < dots.length; i++){
+            if(i === currentIndex){
+                dots[i].classList.add("active");
+            } else {
+                dots[i].classList.remove("active");
+            }
+        }
     }
 };
